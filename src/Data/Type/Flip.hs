b5a1c7cf02014a9a61efd17cf7dbbc8b1879095e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2020-04-28 16:14:42 +0900

    fixity of (<$%>), (<$%>), (>>=%) and (=<<%)

--- src/Data/Type/Flip.hs
+++ src/Data/Type/Flip.hs
@@ -5,6 +5,8 @@ module Data.Type.Flip where
 
 newtype Flip t a b = Flip { unflip :: t b a } deriving Show
 
+infixl 4 <$%>, <*%>
+
 ffmap, (<$%>) :: Functor (Flip t c) => (a -> b) -> t a c -> t b c
 ffmap f = unflip . fmap f . Flip
 (<$%>) = ffmap
@@ -15,8 +17,12 @@ fpure = unflip . pure
 (<*%>) :: Applicative (Flip t c) => t (a -> b) c -> t a c -> t b c
 mf <*%> mx = unflip $ Flip mf <*> Flip mx
 
+infixl 1 >>=%
+
 (>>=%) :: Monad (Flip t c) => t a c -> (a -> t b c) -> t b c
 m >>=% f = unflip $ Flip m >>= Flip . f
+
+infixr 1 =<<%
 
 (=<<%) :: Monad (Flip t c) => (a -> t b c) -> t a c -> t b c
 (=<<%) = flip (>>=%)
